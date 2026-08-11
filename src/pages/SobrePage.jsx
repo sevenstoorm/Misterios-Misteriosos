6843a3d81a80219dacc74c080b906cfe82099474
hakizao6074 <davizinhodjar@gmail.com>
2026-08-11 14:51:03 -0300

    fit: criação inicial do sobre nós

---
+++ src/pages/SobrePage.jsx
@@ -0,0 +1,18 @@
+import SectionHistory from "./Sobre/SectionHistory";
+import SectionMission from "./Sobre/SectionMission";
+import SectionVision from "./Sobre/SectionVision";
+import SectionValue from "./Sobre/SectionValue";
+import SectionGroup from "./Sobre/SectionGroup";
+
+function SobrePage() {
+  return (
+    <main >
+      <SectionHistory />
+      <SectionMission />
+      <SectionVision />
+      <SectionValue />
+      <SectionGroup />
+    </main>
+  );
+}
+export default SobrePage;
